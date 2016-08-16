0f287d56292615cc741aee7642b0d18b2b665f53
Thiago Picanço <thiagolpicanco@gmail.com>
2016-08-15 22:34:54 -0300

    Update GerarRelatorioBean.java

--- GerenciamentoVendas/src/main/java/util/GerarRelatorioBean.java
+++ GerenciamentoVendas/src/main/java/util/GerarRelatorioBean.java
@@ -1,29 +1,4 @@
-/**
- * Copyright (c) 2016 Caixa Econômica Federal. Todos os direitos
- * reservados.
- *
- * Caixa Econômica Federal - siorf
- *
- * Este software foi desenvolvido sob demanda da CAIXA e está
- * protegido por leis de direitos autorais e tratados internacionais. As
- * condições de cópia e utilização do todo ou partes dependem de autorização da
- * empresa. Cópias não são permitidas sem expressa autorização. Não pode ser
- * comercializado ou utilizado para propósitos particulares.
- *
- * Uso exclusivo da Caixa Econômica Federal. A reprodução ou distribuição não
- * autorizada deste programa ou de parte dele, resultará em punições civis e
- * criminais e os infratores incorrem em sanções previstas na legislação em
- * vigor.
- *
- * Histórico do Subversion:
- *
- * LastChangedRevision: $Revision$
- * LastChangedBy: $Author$
- * LastChangedDate: $Date$
- *
- * HeadURL: $HeadURL$
- *
- */
+
 package br.gov.caixa.sitch.relatorio;
 
 import java.io.ByteArrayOutputStream;
@@ -45,19 +20,7 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
 
-/**
- * <p>
- * GerarRelatorioBean
- * </p>
- * <p>
- * Descrição: TODO descrição do tipo
- * </p>
- * <br>
- * <b>Empresa:</b> Cef - Caixa Econômica Federal
- *
- * @author lidoan.andrade
- * @version 1.0
- */
+
 public abstract class GerarRelatorioBean implements Serializable {
 
 	private static final long serialVersionUID = 4288913089814146972L;
